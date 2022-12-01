e67d341340472b65b2533d5417109b0482413c7e
RXMXNDAI <k.romann413@gmail.com>
2022-12-01 19:02:03 +0545

    react app created

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementsByClassName('root'));
+
+root.render(
+  <App />
+)
