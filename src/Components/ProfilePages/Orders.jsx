38bcc9f23362a192e8cd629b36fd0f01e9c0507b
rxmxndai <k.romann413@gmail.com>
2023-01-26 20:40:26 +0545

    nav links use state

---
+++ src/Components/ProfilePages/Orders.jsx
@@ -0,0 +1,24 @@
+import styled from "styled-components"
+
+const Card = styled.div`
+    margin: 20px 50px;
+    box-shadow: rgb(38, 57, 77) 0px 20px 30px -10px;
+    border-radius: 10px;
+    background-color: white;
+    overflow: hidden;
+    width: 100%;
+    height: 600px;
+    display: flex;
+`
+
+const Orders = () => {
+
+
+    return (
+        <Card>
+
+        </Card>
+    )
+}
+
+export default Orders
--- src/Pages/ProfilePage.jsx
+++ src/Pages/ProfilePage.jsx
@@ -1,8 +1,11 @@
 import styled from "styled-components"
 import ProfileNav from "../Components/ProfilePages/ProfileNavItem"
 import ProfileDisplay from "../Components/ProfilePages/ProfileDisplay"
+import Orders from "../Components/ProfilePages/Orders"
+import Account from "../Components/ProfilePages/Account"
 import RightDisplay from "../Components/ProfilePages/Details"
 import { useState } from "react"
+import { useEffect } from "react"
 
 
 
@@ -23,16 +26,33 @@ const ProfilePage = () => {
 
   const titles = [{
     id: "profile",
-    title: "Profile"
+    title: "Profile",
+    component: ProfileDisplay
   }, {
     id: "order",
-    title: "Order"
+    title: "Order",
+    component: Orders
   }, {
     id: "account",
     title: "Account"
   }];
 
   const [active, setActive] = useState("profile")
+  const [component, setComponent] = useState(ProfileDisplay)
+
+  useEffect(() => {
+    switch(active) {
+      case "profile":
+        setComponent(ProfileDisplay)
+      case "orders":
+        setComponent(Orders)
+      case "account":
+        setComponent(Account)
+    }
+    console.log(active);
+  }, [active])
+
+
 
   return (
     <Container>
@@ -48,7 +68,9 @@ const ProfilePage = () => {
           />
         ))}
       </Wrapper>
-      <RightDisplay Component={ProfileDisplay} />
+      <RightDisplay Component={component}
+
+        />
     </Container>
   )
 }
