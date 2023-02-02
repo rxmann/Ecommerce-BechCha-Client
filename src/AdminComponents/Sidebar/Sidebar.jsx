0d9b1fc21755c9fe702fea6661cb8cf73415c1b5
rxmxndai <k.romann413@gmail.com>
2023-02-02 16:13:06 +0545

    Sidebar dynamic

--- src/AdminComponents/Sidebar/Sidebar.jsx
+++ src/AdminComponents/Sidebar/Sidebar.jsx
@@ -1,6 +1,7 @@
 import styled from "styled-components"
-import { LineStyle, TrendingUp, Timeline } from "@mui/icons-material"
+import {  PersonOutline, CreditCard, Store, SettingsApplications, Dashboard, LocalShipping, Category, AccountCircleOutlined, ExitToApp, NotificationsNone } from "@mui/icons-material"
 import "../support.css"
+import { useState } from "react"
 
 const Container = styled.div`
     flex: 1;
@@ -8,7 +9,7 @@ const Container = styled.div`
     max-width: 250px;
     position: sticky;
     top: 70px;
-    background-color: #f5f7f8;
+    border-right: 0.5px solid #f5f7f8;
 `
 
 const SidebarWrapper = styled.div`
@@ -21,8 +22,10 @@ const SidebarMenu = styled.div`
 `
 
 const SidebarTitle = styled.h3`
-    font-size: 14px;
-    margin-bottom: 10px;
+    font-size: 10px;
+    font-weight: bold;
+    color: #999;
+    margin: 15px 0px 10px 0px;
 `
 
 const SidebarList = styled.ul`
@@ -37,38 +40,107 @@ const SidebarListItem = styled.li`
     padding: 5px 10px;
     cursor: pointer;
     display: flex;
-    font-size: 16px;
+    font-size: 14px;
     align-items: center;
+    color: #0171b6;
 
 
     &:hover {
         background-color: #e2f5ff;
+        color: #333333;
     }
 `
 
 
 const Sidebar = () => {
 
+    const [selected, setSelected] = useState("");
+
+    const data = [
+        {
+            title: "MAIN",
+            options: [{
+                name: "Dashboard",
+                icon: (<Dashboard />),
+                selected: selected
+            }],
+        },
+        {
+            title: "CONTROLS",
+            options: [{
+                name: "Users",
+                icon: (<PersonOutline />),
+                selected: selected
+            }],
+        },
+        {
+            title: "PRODUCTS",
+            options: [{
+                name: "Products",
+                icon: (<Store />),
+                selected: selected
+            }, {
+                name: "Categories",
+                icon: (<Category />),
+                selected: selected
+            }],
+        },
+        {
+            title: "SERVICES",
+            options: [{
+                name: "Orders",
+                icon: (<CreditCard />),
+                selected: selected
+            },
+            {
+                name: "Delivery",
+                icon: (<LocalShipping />),
+                selected: selected
+            },
+            {
+                name: "Notification",
+                icon: (<NotificationsNone />),
+                selected: selected
+            }],
+        },
+        {
+            title: "USER",
+            options: [{
+                name: "Profile",
+                icon: (<AccountCircleOutlined />),
+                selected: selected
+            },
+            {
+                name: "Settings",
+                icon: (<SettingsApplications />),
+                selected: selected
+            },
+            {
+                name: "Logout",
+                icon: (<ExitToApp />),
+                selected: selected
+            }],
+        },
+    ]
+
 
     return (
         <Container>
             <SidebarWrapper>
-                <SidebarMenu>
-                    <SidebarTitle> Dashboard </SidebarTitle>
-                    <SidebarList>
-                        <SidebarListItem className="menuActive">
-                            <LineStyle className="iconMUI"/>  Home
-                        </SidebarListItem>
 
-                        <SidebarListItem>
-                        <Timeline  className="iconMUI"/>  Analytics
-                        </SidebarListItem>
-
-                        <SidebarListItem>
-                         <TrendingUp className="iconMUI" />  Sales
-                        </SidebarListItem>
-                    </SidebarList>
-                </SidebarMenu>
+                <SidebarList>
+                        {data.map( (menu) =>  (
+                            <SidebarMenu>
+                                <SidebarTitle > {menu.title} </SidebarTitle>
+                                {menu.options.map( (child) => (
+                                    <SidebarListItem className="iconMUI"> 
+                                        {child.icon}
+                                        {child.name} 
+                                    </SidebarListItem>
+                                ))} 
+                            </SidebarMenu>
+                        ))}
+                </SidebarList>
             </SidebarWrapper>
         </Container>
     )
