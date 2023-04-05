36f4a75f7a2e71d3ec57d8e318fe898db95c3573
rxmandai <k.romann413@gmail.com>
2023-04-05 21:32:20 +0545

    Invoice page layout drawn

---
+++ src/Components/User/Checkout/InvoicePage.jsx
@@ -0,0 +1,32 @@
+import styled from "styled-components"
+
+const Container = styled.div`
+
+`
+
+
+const InvoicePage = () => {
+  return (
+    <Container>
+        <TopWrapper>
+
+        </TopWrapper>
+
+        <OrderInfoWrapper>
+
+        </OrderInfoWrapper>
+
+        <OrderList>
+
+        </OrderList>
+
+        <Total>
+
+        </Total>
+
+        <Footer> www.bechcha.com.np </Footer>
+    </Container>
+  )
+}
+
+export default InvoicePage
